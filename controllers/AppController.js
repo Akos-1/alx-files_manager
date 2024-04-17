0ae0072e4808d40b032337c298fcb082cb63f96e
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-04-17 20:58:26 +0000

    Updated AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,20 +1,22 @@
-// controllers/AppController.js
-
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
+import RedisClient from '../utils/redis';
+import DBClient from '../utils/db';
 
 class AppController {
-    static async getStatus(req, res) {
-        const redisStatus = redisClient.isAlive();
-        const dbStatus = dbClient.isAlive();
-        res.status(200).json({ redis: redisStatus, db: dbStatus });
-    }
+  static getStatus(req, res) {
+    const data = {
+      redis: RedisClient.isAlive(),
+      db: DBClient.isAlive(),
+    };
+    return res.status(200).send(data);
+  }
 
-    static async getStats(req, res) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
-        res.status(200).json({ users: usersCount, files: filesCount });
-    }
+  static async getStats(req, res) {
+    const data = {
+      users: await DBClient.nbUsers(),
+      files: await DBClient.nbFiles(),
+    };
+    return res.status(200).send(data);
+  }
 }
 
 module.exports = AppController;
